ee7e523789691c5b4f1341f85ecb28f5253d1e43
Matthew Fisher <matthewf@opdemand.com>
2014-08-01 13:37:10 -0700

    fix(logger): use jeromer/syslogparser
    This library parses the incoming syslog message properly, replacing the
    spaghetti code which would incorrectly parse the incoming packet.

--- logger/syslog/message.go
+++ logger/syslog/message.go
@@ -2,43 +2,25 @@ package syslog
 
 import (
 	"fmt"
-	"net"
+	"log/syslog"
 	"time"
 )
 
 type Message struct {
-	Time   time.Time
-	Source net.Addr
-	Facility
-	Severity
-	Timestamp time.Time // optional
-	Hostname  string    // optional
-	Tag       string    // message tag as defined in RFC 3164
-	Content   string    // message content as defined in RFC 3164
-	Tag1      string    // alternate message tag (white rune as separator)
-	Content1  string    // alternate message content (white rune as separator)
-}
-
-// NetSrc only network part of Source as string (IP for UDP or Name for UDS)
-func (m *Message) NetSrc() string {
-	switch a := m.Source.(type) {
-	case *net.UDPAddr:
-		return a.IP.String()
-	case *net.UnixAddr:
-		return a.Name
-	case *net.TCPAddr:
-		return a.IP.String()
-	}
-	// Unknown type
-	return m.Source.String()
+	Time      time.Time
+	Priority  syslog.Priority
+	Timestamp time.Time
+	Hostname  string
+	Tag       string
+	Content   string
 }
 
 func (m *Message) String() string {
 	timeLayout := "2006-01-02 15:04:05"
 	return fmt.Sprintf(
-		"%s %s %s%s",
+		"<%d>%s %s: %s",
+		m.Priority,
 		m.Time.Format(timeLayout),
-		m.Hostname,
 		m.Tag,
 		m.Content,
 	)
